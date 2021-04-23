d7539325a0234d8c0dad71cd7559fe7775c732b9
Sojasmine <sojasmine.gjerstad@gmail.com>
2021-04-23 10:24:59 +0000

    :construction: Added a new audio folder, styled CSS, start coding on the javascript file, added overlay text on index.html.

---
+++ assets/script/main.js
@@ -0,0 +1,12 @@
+/*jshint esversion: 6 */
+if(document.readyState === 'loading') {
+    document.addEventListener('DOMcontentLoaded', ready());
+} else {
+    ready();
+}
+
+function ready() {
+    let overlays = Array.from(document.querySelectorAll(".overlay-text"));
+    
+
+}

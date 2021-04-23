8e106c1712dce28932c92ac3027f1a3d77eeafa8
Sojasmine <sojasmine.gjerstad@gmail.com>
2021-04-23 17:12:29 +0000

    :construction: Worked on the Javscript and made some change at index.html.

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,18 +1,13 @@
 /*jshint esversion: 6 */
-if(document.readyState === 'loading') {
-    document.addEventListener('DOMcontentLoaded', ready());
-} else {
-    ready();
-}
 
-function startGame() {
-    alert(1234); 
-    /*write a message here*/
-}
+// if (document.readyState === 'loading') {
+//     document.addEventListener('DOMcontentLoaded', ready());
+// } else {
+//     ready();
+// }
 
 let defaultAlt = "Lemur",
-defaultImg ="assets/images/lemur7.jpg",
-//easy
+    defaultImg = "assets/images/lemur7.jpg",
 easy = [{
     src: "assets/images/cameleon.jpg",
     alt: "Cameleon"
@@ -34,146 +29,61 @@ easy = [{
     alt: "Fosa"
 },{
     src: "assets/images/maki.jpg",
-    alt: "maki"
+    alt: "Maki"
 },{
     src: "assets/images/maki.jpg",
-    alt: "maki"
+    alt: "Maki"
 }],
-//medium
+
+
 medium = [{
-    src: "assets/images/floodhest.jpg",
-    alt: "floodhest"
-},
-{
-    src: "assets/images/floodhest.jpg",
-    alt: "floodhest"
+    src: "assets/images/makoa.jpg",
+    alt: "Makoa"
 },{
-    src: "assets/images/make.jpg",
-    alt: "Make"
-},{
-    src: "assets/images/make.jpg",
-    alt: "Make"
-},{
-    src: "assets/images/fosa.jpg",
-    alt: "Fosa"
-},{
-    src: "assets/images/fosa.jpg",
-    alt: "Fosa"
-},{
-    src: "assets/images/maki.jpg",
-    alt: "Maki"
-},{
-    src: "assets/images/maki.jpg",
-    alt: "Maki"
+    src: "assets/images/makoa.jpg",
+    alt: "Makoa",
 },{
     src: "assets/images/lion.jpg",
     alt: "Lion"
 },{
-    src: "assets/images/lion.jpg",
-    alt: "Lion"
-},{
-    src: "assets/images/pinguin.jpg",
-    alt: "Pinguin"
-},{
-    src: "assets/images/pinguin.jpg",
-    alt: "Pinguin"
+    src: "assets/images/make.jpg",
+    alt: "Make",
 }],
-//hard
+
+
+
 hard = [{
-    src: "assets/images/maka.jpg",
-    alt: "Maka"
-},
-{
-    src: "assets/images/maka.jpg",
-    alt: "Maka"
-},{
-    src: "assets/images/makoa.jpg",
-    alt: "Makoa"
-},{
-    src: "assets/images/makoa.jpg",
-    alt: "Makoa"
-},{
-    src: "assets/images/fosa.jpg",
-    alt: "Fosa"
-},{
-    src: "assets/images/fosa.jpg",
-    alt: "Fosa"
-},{
-    src: "assets/images/maki.jpg",
-    alt: "Maki"
-},{
-    src: "assets/images/maki.jpg",
-    alt: "Maki"
-},{
-    src: "assets/images/papegoja.jpg",
-    alt: "Papegoja"
-},{
-    src: "assets/images/papegoja.jpg",
-    alt: "Papegoja"
+    src: "assets/images/pinguin.jpg",
+    alt: "Pinguin"
 },{
     src: "assets/images/pinguin.jpg",
-    alt: "Giraf"
-},{
-    src: "assets/images/pinguin.jpg",
-    alt: "Giraf"
-},{
-    src: "assets/images/lion.jpg",
-    alt: "Lion"
-},{
-    src: "assets/images/lion.jpg",
-    alt: "Lion"
-},{
+    alt: "Pinguin",
     src: "assets/images/floodhest.jpg",
     alt: "Floodhest"
 },{
-    src: "assets/images/flodhest.jpg",
-    alt: "Floodhest"
+    src: "assets/images/floodhest.jpg",
+    alt: "Floodhest",
 }];
 
 
 function startGame() {
-    //check and get the value of the radiobutton selected
-level = GetSelectedLevel();
+    setTimeout(function(){ alert("Please select a level!"); }, 100);
+    level = getSelectedLevel();
 
-switch (level) {
-    case "easy":
-        BuildGrid(8, easy);
-        break;
+    switch (level) {
+        case "easy":
+            BuildGrid(8, easy);
+            break;
 
-    case "medium":
-        BuildGrid(12, medium);
-        break;
+        case "medium":
+            BuildGrid(12, medium);
+            break;
 
-    case "hard":
-        BuildGrid(16, hard)
-}
-}
+        case"hard":
+            BuildGrid(16, hard);
 
-function GetSelectedLevel() {
-    let selectedLevel = null,
-    ele = document.getElementsByName('level');
-
-    for (i=0; i < ele.length; i++) {
-        if (ele[i].checked)
-        GetSelectedLevel= ele[i].nodeValue;
+        default:
+            break;
     }
-    return selectedLevel;
 }
 
-function BuildGrid(totalBoxes, imgArray) {
-    gridContent = "";
-
-    //Loop through the images Array passed in
-    imgArray.forEach(img=> {
-
-        //Generate the HTML needed
-    gridContent += `<div class="memory-game" data-framework="${img.alt}">
-    <img class="front-face" src="${img.src}" alt="${img.alt}" />
-    <img class="back-face" src="${defaultImg}" alt="${defaultAlt}" />
-</div>`;
-        
-    });
-
-    //Append the generated HTML to the body of the container.
-    document.getElementById("ImagesGrid").innerHTML = gridContent;
-}
